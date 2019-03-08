7843978e9885c70af26471738d3973c27bb9fbed
Cheng Vue <csalvue@gmail.com>
2019-03-08 03:05:16 +0000

    Automatically backed up by Learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -1,7 +1,7 @@
 class SongsWorker
 
-  def perform(songs_file)
-    CSV.foreach(params["songs_file"].path, headers: true) do |song|
+  def perform(file)
+    CSV.foreach(params["file"].path, headers: true) do |song|
       Song.create(title: song[0], artist_name: song[1])
     end
   end
